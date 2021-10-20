460dc19486f5ca04e57cd34debcdc69cf1229e81
HarshRajAmbastha <harsh_ra@ce.iitr.ac.in>
2021-10-20 10:58:48 +0530

    about page further modified

--- spark-frontend/src/components/about/About.js
+++ spark-frontend/src/components/about/About.js
@@ -37,29 +37,76 @@ export default function About() {
   }, [])
 
     return (
-        <div className="abt-body">
-            <div className=" d-flex justify-content-center align-items-center about">
+//         <div className="abt-body">
+//             <div className=" d-flex justify-content-center align-items-center about">
                 
-                Name:{sessionStorage.getItem("name")}
-                <br />
-                Id:{sessionStorage.getItem("id")}
-                <br />
-                Email:{sessionStorage.getItem("email")}
-                <br />
-                Phone:{sessionStorage.getItem("phone")}
-                <br />
-                Work:{sessionStorage.getItem("work")}
+//                 Name:
+//                 <br />
+//                 Id:{sessionStorage.getItem("id")}
+//                 <br />
+//                 Email:{sessionStorage.getItem("email")}
+//                 <br />
+//                 Phone:{sessionStorage.getItem("phone")}
+//                 <br />
+//                 Work:{sessionStorage.getItem("work")}
                 
-                
-
                 
 
-            </div>
+                
+
+//             </div>
            
+
+//                 <div class="card-footer text-muted d-flex justify-content-center align-items-center">
+//     Designed and Developed by : Harsh Raj Ambastha
+//   </div>
+//         </div>
+<div className="abt">
+<div className="card1 d-flex justify-content-center align-items-center">
+    <div className="pic ">
+        <img className="avtaar" src="https://www.pinclipart.com/picdir/middle/379-3797946_software-developer-computer-servers-web-others-web-developer.png" alt="" />
+        <div className="name">{sessionStorage.getItem("name")}</div>
+        <div className="work">{sessionStorage.getItem("work")}</div>
+    </div>
+    <div className="details">
+    <div class="container my-3">
+  <div class="row my-1  a-row">
+    <div class="col a-col">
+      UserID:
+    </div>
+    <div class="col a-col">
+    {sessionStorage.getItem("id")}
+    </div>
+    
+  </div>
+  <div class="row my-1 a-row">
+    <div class="col a-col ">
+     Email ID:
+    </div>
+    <div class="col a-col">
+    {sessionStorage.getItem("email")}
+    </div>
+    
+  </div>
+  <div class="row my-1 a-row">
+    <div class="col a-col">
+      Contact No:
+    </div>
+    <div class="col a-col">
+    {sessionStorage.getItem("phone")}
+    </div>
+    
+  </div>
+</div>  
+    </div>
+        
+    
+</div>
 
                 <div class="card-footer text-muted d-flex justify-content-center align-items-center">
     Designed and Developed by : Harsh Raj Ambastha
   </div>
-        </div>
+
+</div>
     )
 }
